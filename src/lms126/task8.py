0ec434b51e9b7e569a1c42a67f6dbb4b2253ba93
Thinsuu <thinyadanarsu@gmail.com>
2023-02-06 23:39:25 +0100

    Solution task 8

--- src/lms126/task8.py
+++ src/lms126/task8.py
@@ -2,4 +2,14 @@ from to_do import TODO
 
 
 def task8():
-    return TODO("Erase this line and write your solution")
+    return """
+    FOR number is X
+    IF X is multiple of 3 THEN
+    OUTPUT Foo;
+    IF X is multiple of 5 THEN
+    OUTPUT Bar;
+    IF X is multiple of both 3 and 5 THEN
+    OUTPUT FooBar;
+    IF X is not multiple of 3 or 5 THEN
+    OUTPUT Qix
+    """
