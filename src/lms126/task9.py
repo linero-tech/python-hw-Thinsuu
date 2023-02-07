71554e20fbd81f5ad49062ed1f93e41c9fde5733
Thinsuu <thinyadanarsu@gmail.com>
2023-02-07 23:14:45 +0100

    solution task 9

--- src/lms126/task9.py
+++ src/lms126/task9.py
@@ -2,4 +2,25 @@ from to_do import TODO
 
 
 def task9():
-    return TODO("Erase this line and write your solution")
+    return """
+    INPUT Show the user to put his/her bank card into the 
+    green light case shown on the right side of the screen
+    IF the card is both valid and pushed through properly 
+    THEN proceed further to ask for pincode
+    ELSE push out the card from the case
+    INPUT Ask for the pincode of the user's bank card
+    INPUT LET the user choose and click the following options:
+    1) withdraw the money from the card,
+    2) put the money into the card,
+    3) check the balance amount of the card,
+    4) change the pin code
+    5) language setting
+    IF the user choose the option no. 1 THEN proceed to ask user to 
+    to type the desirable amount of withdrawal ELSE proceed ...
+    INPUT Ask the user to click Ok to confirm the amount
+    INPUT Start ATM's internal process to withdraw money from card
+    and convert to cash
+    INPUT Notify the ATM user by blinking and pushing out cash 
+    under the screen
+    OUTPUT Push the bank card out
+    """
