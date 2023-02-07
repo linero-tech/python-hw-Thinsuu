e8589c93b060914cad7df33950484105f672622e
Thinsuu <thinyadanarsu@gmail.com>
2023-02-08 00:48:39 +0100

    Solution task 10

--- src/lms126/task10.py
+++ src/lms126/task10.py
@@ -2,4 +2,19 @@ from to_do import TODO
 
 
 def task10():
-    return TODO("Erase this line and write your solution")
+    return """
+    Go to the browser and type the page name or keyword to 
+    create an email
+    Click the create an account
+    Enter first name and last name
+    Set desirable account address
+    IF the account address is taken or invalid THEN it will be 
+    notified and given  suggested address ELSE the user can 
+    fill in until the account address is valid and new
+    Set the password with more than six characters as 
+    the requirement
+    IF the password length is less than 6 characters <6 THEN 
+    reset until it is valid ELSE confirm the password by 
+    entering the same password
+    Click Next
+    """
