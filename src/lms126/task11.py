43159ee83b79d4e10f3150902c79a32353c13955
Thinsuu <thinyadanarsu@gmail.com>
2023-02-08 15:09:25 +0100

    Solution task 11

--- src/lms126/task11.py
+++ src/lms126/task11.py
@@ -2,4 +2,13 @@ from to_do import TODO
 
 
 def task11():
-    return TODO("Erase this line and write your solution")
+    return """
+    INPUT temperature in Celsius (C)
+    Calculate Fahrenheit (F) as Celsius multiply 9/5 and 
+    add 32 which in turns F = (C*9/5)+32
+    OUTPUT F
+    INPUT temperature in Fahrenheit (F)
+    Calculate Celsius (C) as Fahrenheit minus 32 and multiply
+    5/9 which in turns C = (F-32)*5/9
+    OUTPUT C
+    """
